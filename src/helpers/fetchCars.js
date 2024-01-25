0026da5e006bcd9d330627ce627b76d507a264f8
Yriy Fedchenko <y.fedchenko@gmail.com>
2024-01-25 20:52:29 +0200

    add pagination

--- src/helpers/fetchCars.js
+++ src/helpers/fetchCars.js
@@ -2,10 +2,9 @@ import axios from 'axios';
 
 const baseUrl = 'https://6553c49e5449cfda0f2f265b.mockapi.io/';
 
-export const fetchCars = async endpoint => {
+export const fetchCars = async (endpoint, page, limit) => {
   try {
-    const response = await axios.get(`${baseUrl}${endpoint}`);
-    // console.log(response.data);
+    const response = await axios.get(`${baseUrl}${endpoint}?page=${page}&limit=${limit}`);
     return response.data;
   } catch (error) {
     console.error(`Error fetching data: ${error}`);
--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -2,17 +2,31 @@ import CarList from '../../components/CarList/CarList';
 import CarFilter from '../../components/CarFilter/CarFilter';
 import { useEffect, useState } from 'react';
 import { fetchCars } from '../../helpers/fetchCars';
+import Button from '@mui/material/Button';
 
 const Catalog = () => {
   const [cars, setCars] = useState([]);
   const [filteredCars, setFilteredCars] = useState([]);
+  const [page, setPage] = useState(1);
+  const limit = 10;
+
+  const loadMoreCars = async () => {
+    try {
+      const newCars = await fetchCars('advert', page, limit);
+      setCars(prevCars => [...prevCars, ...newCars]);
+      setPage(prevPage => prevPage + 1);
+    } catch (error) {
+      console.error('Failed to load more cars:', error);
+    }
+  };
 
   useEffect(() => {
     async function fetchSearchCars() {
       try {
-        const data = await fetchCars('/advert');
+        const data = await fetchCars('advert', page, limit);
         setCars(data);
         setFilteredCars(data);
+        setPage(prevPage => prevPage + 1);
       } catch (error) {
         console.error(error);
       }
@@ -24,6 +38,21 @@ const Catalog = () => {
     <section className="container">
       <CarFilter setFilteredCars={setFilteredCars} cars={cars} />
       <CarList cars={filteredCars} />
+      <Button
+        sx={{
+          fontFamily: 'Manrope',
+          fontSize: 14,
+          fontWeight: 600,
+          paddingTop: 2.06,
+          paddingBottom: 2.06,
+          paddingLeft: 5.5,
+          paddingRight: 5.5,
+        }}
+        variant="contained"
+        onClick={loadMoreCars}
+      >
+        Search
+      </Button>
     </section>
   );
 };
