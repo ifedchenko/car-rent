3b9d52fe0c5b8536494a1e1b463f3debc3d09782
Yriy Fedchenko <y.fedchenko@gmail.com>
2024-01-26 17:02:54 +0200

    add loading

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,18 +1,24 @@
 import CarList from '../../components/CarList/CarList';
+import { Puff } from 'react-loader-spinner';
+import Box from '@mui/material/Box';
 import { useEffect, useState } from 'react';
 import { fetchCars } from '../../helpers/fetchCars';
 
 const Favorite = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const [cars, setCars] = useState([]);
   const [favoriteCars, setFavoriteCars] = useState([]);
 
   useEffect(() => {
     async function fetchSearchCars() {
       try {
+        setIsLoading(true);
         const data = await fetchCars('adverts');
         setCars(data);
       } catch (error) {
         console.error(error);
+      } finally {
+        setIsLoading(false);
       }
     }
 
@@ -29,7 +35,17 @@ const Favorite = () => {
 
   return (
     <section className="container">
-      <CarList cars={favoriteCars} />
+      {isLoading ? (
+        <Box
+          sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}
+        >
+          <Puff loading={isLoading} size={100} color="#1976d2" />
+        </Box>
+      ) : (
+        <>
+          <CarList cars={favoriteCars} />
+        </>
+      )}
     </section>
   );
 };
